d4c0ee0bf19e5578e07425465930e1004cbe16d7
LennonChin <i@coderap.com>
2018-04-15 01:00:39 +0800

    1, modified custom nav link model;

--- apps/base/models.py
+++ apps/base/models.py
@@ -9,10 +9,18 @@ class NavigationLink(models.Model):
     """
     自定义导航
     """
+    TARGET_TYPE = (
+        ("_blank", "blank - 浏览器总在一个新打开、未命名的窗口中载入目标文档。"),
+        ("_self", "self - 这个目标的值对所有没有指定目标的 <a> 标签是默认目标，它使得目标文档载入并显示在相同的框架或者窗口中作为源文档。这个目标是多余且不必要的，除非和文档标题 <base> 标签中的 target 属性一起使用。"),
+        ("_parent", "parent - 这个目标使得文档载入父窗口或者包含来超链接引用的框架的框架集。如果这个引用是在窗口或者在顶级框架中，那么它与目标 _self 等效。"),
+        ("_top", "top - 这个目标使得文档载入包含这个超链接的窗口，用 _top 目标将会清除所有被包含的框架并将文档载入整个浏览器窗口。")
+    )
     name = models.CharField(max_length=30, verbose_name="名称", help_text="名称")
     desc = models.CharField(max_length=100, verbose_name="简介", help_text="简介")
     image = models.ImageField(upload_to="base/friendlink/image/%y/%m", null=True, blank=True, verbose_name="图片", help_text="图片")
-    url = models.URLField(max_length=200, verbose_name="链接", help_text="链接")
+    url = models.CharField(max_length=200, verbose_name="链接", help_text="链接")
+    target = models.CharField(max_length=10, choices=TARGET_TYPE, null=True, blank=True, verbose_name="Target类别",
+                                 help_text="对应于a标签中的target属性")
     add_time = models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name="添加时间", help_text="添加时间")
 
     class Meta:
